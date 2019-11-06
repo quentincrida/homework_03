af33fe2df7a4c0d97515517f5a2256225e248413
Quentin Crida <qcrida@gmail.com>
2019-11-06 18:05:05 +0000

    stops finished

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -1,20 +1,32 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 
-# 9. Reverse the positions of the stops in the array
+
 # 10. Print out all the stops using a for loop
 #
-#stops.push("Edinburgh")
-#stops.unshift("Glasgow Queen St")
-#stops.insert(4,"Polmont")
-#p stops.index("Linlithgow")
-#stops.delete("Livingston")
-#p stops
-#stops.delete_at(4)
-#p stops
-#stops.delete_at(1)
-#p stops
-#p stops.count
- #p stops.at(2)
-  #p stops.at(-4)
+stops.push("Edinburgh")
+
+stops.unshift("Glasgow Queen St")
+
+stops.insert(4,"Polmont")
+
+p stops.index("Linlithgow")
+
+stops.delete("Livingston")
+p stops
+
+stops.delete_at(4)
+p stops
+
+stops.delete_at(1)
+p stops
+
+p stops.count
+ p stops.at(2)
+  p stops.at(-4)
+
 p stops.reverse
+
+for stop in stops
+  p stop
+end
