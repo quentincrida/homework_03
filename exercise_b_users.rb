ec3829fc3a1749d2149b17db328b004416641931
Quentin Crida <qcrida@gmail.com>
2019-11-06 20:32:47 +0000

    even numbers

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -58,7 +58,8 @@ p users["Erik"][:home_town]
 p users["Erik"][:lottery_numbers]
 p users["Avril"][:pets][0][:species]
 p users["Erik"][:lottery_numbers].min
-#p users["Avril"][:lottery_numbers]
+p users["Avril"][:lottery_numbers].select { |num| num.even?}
+
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
 # 2. Get Erik's hometown
 # 3. Get the array of Erik's lottery numbers
